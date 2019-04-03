c37c4ca072040a720bbc8bf731438196dffd0140
João Marques <joao.marques.contacto@gmail.com>
2019-04-03 14:58:25 +0000

    Change DHT_Node and start Receptionist.

---
+++ Receptionist.py
@@ -0,0 +1,7 @@
+import utils
+
+class Receptionist(DHT_Node):
+    def __init__(self, address, dht_address=None, timeout=3):
+        super().__init__(self, addres, dht_address, timeout, RECEPTIONIST_ID)
+
+    def 
