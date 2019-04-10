3f60d7e7062f66e0478576d15d5799776a8a31ce
tom1k <tomascosta@ua.pt>
2019-04-10 16:52:47 +0100

    Added Employee

---
+++ Employee.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+import time
+import pickle
+import socket
+import logging
+# import argparse
+import threading
+import Node
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M:%S')
+
+class Employee(Node):
+	def __init__(self)
+		super().__init__(self, 3, address, timeout=3)
+		self.queueIn = queue.Queue
+
+
+def main(port, ring, timeout):
+
+
+if __name__ == '__main__':
