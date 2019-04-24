eb9cb0240a476012c9c3648727c3a5b215554f15
João Marques <jota@aluno-7335.wireless.ua.pt>
2019-04-24 17:07:34 +0100

    Delete unnecessary stuff from Node

--- Node.py
+++ Node.py
@@ -137,49 +137,20 @@ class Node(threading.Thread):
                 elif o['method'] == 'NODE_DISCOVERY':
                     self.propagate_table(o['args'])   
 
-                #elif o['method'] == 'ORDER': # need to wrap in TOKEN
-                #    msg = { 'method' : 'TOKEN' , 
-                #            'args' : { 'method' : o['method'], 
-                #                       'args' : { 'client_addr' : addr, 
-                #                                  'order' : o['args'] }, 
-                #                       'dest_id' : self.node_table['Receptionist'] }}
-                #    self.send(self.successor_address, msg)
-
-                #elif o['method'] == 'PICKUP': # need to wrap in TOKEN
-                #    msg = { 'method' : 'TOKEN' , 
-                #            'args' : { 'method' : o['method'], 
-                #                       'args' : { 'client_addr' : addr, 
-                #                                  'order' : o['args'] }, 
-                #                       'dest_id' : self.node_table['Employee'] }}
-                #    self.send(self.successor_address, msg)
-
                 elif o['method'] == 'TOKEN': # send to worker
                    if o['args']=='EMPTY':
                        if not self.queueOut.empty():
                            nextMessage = self.queueOut.get()
                            if nextMessage != None:
                                # wrap in TOKEN
-                               if nextMessage['method'] == 'ORDER_RECVD':
-                                   self.send(nextMessage['args']['client_addr'], nextMessage)
-
-                               elif nextMessage['method']=='DELIVER': #enviar pra cliente caso method seja deliver
-                                   msg = { 'method':'DELIVER' ,
-                                           'args': { 'ticket': nextMessage['args']['orderTicket']
-                                           }}
-
-                                   clientAddress = nextMessage['args']['client_addr']
-                                   self.logger.debug('Sending client %s food', nextMessage['args']['orderTicket'])
-                                   self.send(clientAddress, msg)
-
-                               else:
-                                   msg = { 'method' : 'TOKEN', 'args' : nextMessage }
-                                   msg['args']['dest_id'] = self.node_table[nextMessage['args']['dest']]
-                        
-                                   #permite mais que um cozinheiro, pois enviamos na mensagem que cozinheiro e que pediu e podemos-lhe responder
-                                   if nextMessage['method'] == 'EQPT_REQ':
-                                       msg['args']['args']['cookReq'] = self.node_table[nextMessage['args']['cook']]
-                                   self.send(self.successor_address, msg)
-                                   self.logger.debug('Sending Token: %s', nextMessage['method'])
+                               msg = { 'method' : 'TOKEN', 'args' : nextMessage }
+                               msg['args']['dest_id'] = self.node_table[nextMessage['args']['dest']]
+                    
+                               #permite mais que um cozinheiro, pois enviamos na mensagem que cozinheiro e que pediu e podemos-lhe responder
+                               if nextMessage['method'] == 'EQPT_REQ':
+                                   msg['args']['args']['cookReq'] = self.node_table[nextMessage['args']['cook']]
+                               self.send(self.successor_address, msg)
+                               self.logger.debug('Sending Token: %s', nextMessage['method'])
                        else:
                            self.send(self.successor_address, o)
                    elif o['args']['dest_id']==self.own_id:
